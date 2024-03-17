40c7935a3ae4b0d06155de937aa5d46330b20aba
Imran Hasan <imran.h94@gmail.com>
2024-03-18 01:38:15 +0500

    added logic to manage list of goals

--- App.js
+++ App.js
@@ -3,13 +3,19 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
 
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText);
   }
 
+  // value for currentCourseGoals will be provided automatically by reach when it calls this updating function
   function addGoalHandler() {
-    console.log(enteredGoalText);
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+    // setGoalList([...goalList, enteredGoalText]) // not a good way of updating state of something that is based on previous state
   }
 
   return (
@@ -23,7 +29,9 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        {courseGoals.map((goal) => (
+          <Text key={goal}>{goal}</Text>
+        ))}
       </View>
     </View>
   );
